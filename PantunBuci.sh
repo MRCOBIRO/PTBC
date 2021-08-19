81304b190c19bd85fc6dad1e2dab6cb2af36bd9c
Mr Cobiro <87790524+MRCOBIRO@users.noreply.github.com>
2021-08-19 13:12:08 +0700

    Add files via upload

---
+++ PantunBuci.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# code Warna
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+white='\033[37;1m'
+
+# tampilan
+clear
+figlet PantunBucin
+echo -e "\033[31;1m==========================================================="
+echo -e "\033[33;1m Author : Mr Cobiro"
+echo -e "\033[33;1m Team   : Mafia Teknologi"
+echo -e "\033[31;1m==========================================================="
+echo
+echo -e "\033[36;1m"
+read -p "MASUKKAN PASSWORD DULU NGAB : " Pas
+if [ $Pas = 92722 ]
+then
+clear
+sleep 2
+echo -e "1)."
+echo -e "Duduk Santai Membaca Buku" | lolcat
+echo -e "Baca Buku Cerita Cinta" | lolcat
+echo -e "Saat Aku Bertemu Kamu" | lolcat
+echo -e "Ehh Aku Langsung Jatuh Cinta" | lolcat
+echo
+echo -e "2)."
+echo -e "Buah Manggis" | lolcat
+echo -e "Buah Kedondong" | lolcat
+echo -e "Pacar Nangis" | lolcat
+echo -e "Minta Di Gendong" | lolcat
+echo
+echo -e "3)."
+echo -e "Beli Baju Di Semarang" | lolcat
+echo -e "Bayar Dulu Baru Pulang" | lolcat
+echo -e "Sungguh Lucu Cewek Sekarang" | lolcat
+echo -e "Di Cium Sekali Minta Di Ulang" | lolcat
+echo
+echo -e "4)."
+echo -e "Baca Buku Cerita Cinta" | lolcat
+echo -e "Bacanya Sembunyi Di Goa" | lolcat
+echo -e "Kalau Kau Benar-Benar Cinta" | lolcat
+echo -e "Berjanji lah untuk setia" | lolcat
+echo
+echo -e "5)."
+echo -e "Jalan Jalan Di Gunung Jati" | lolcat
+echo -e "Kulihat Rumah Berdinding Jati" | lolcat
+echo -e "Biar Ku Mati Ditindis Jati" | lolcat
+echo -e "Asal Ku Dapat Cewek Sejati"| lolcat
+echo
+echo -e "6)."
+echo -e "Buah Nangka" | lolcat
+echo -e "Buah Semangka" | lolcat
+echo -e "Tidak Nyangka" | lolcat
+echo -e "Yang Baca Manis Juga" | lolcat
+echo
+echo -e "7)."
+echo -e "Jalan-Jalan Ke Eropa" | lolcat
+echo -e "Jangan Lupa Ke Belgia" | lolcat
+echo -e "Wahai Kamu Cantik Rupa" | lolcat
+echo -e "Please Terimah Saya" | lolcat
+echo
+echo -e "8)."
+echo -e "Kalau Nyebrang Jalan" | lolcat
+echo -e "Jangan Lupa Lihat Kanan" | lolcat
+echo -e "Walau Kita Pelan-Pelan" | lolcat
+echo -e "Pasti Sampai Ke Pelaminan" | lolcat
+echo
+echo -e "9)."
+echo -e "Mencari Barang Di Atas Kayu" | lolcat
+echo -e "Barang Yang Hilang Sudah Ketemu" | lolcat
+echo -e "Setiap Kali Aku Memandang  Mu" | lolcat
+echo -e "Ingin Sekali Aku Berkata i love you" | lolcat
+echo
+echo -e "10)."
+echo -e "Pergi Ke Pasar Beli Jambu" | lolcat
+echo -e "Pergi Ke Taman lihat Kupu-Kupu" | lolcat
+echo -e "Kenali Aku Pada Orang Tua Mu" | lolcat
+echo -e "Agar Aku Bisa Minta Restu" | lolcat
+echo
+echo -e "11)."
+echo -e "Beli Batik Beli Manggis" | lolcat
+echo -e "Makan Salak Di Rumah Mu" | lolcat
+echo -e "Adek Cantik Dan Manis" | lolcat
+echo -e "BolehKan Aku Mencintai Mu" | lolcat
+echo
+echo -e "12)."
+echo -e "Pohon Sagu Jatuh Di Tebang" | lolcat
+echo -e "Pohon Duku Di Bikin Sarang" | lolcat
+echo -e "Jangan Ragu Jangan Bimbang" | lolcat
+echo -e "Cintaku Hanya Untuk Mu Seorang" | lolcat
+echo
+fi
